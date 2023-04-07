319b5d947dccc9e9a4ede47d404016025ba8f7d4
dhrifa <dhrifa.kherroubi@mobileconsultingsolutions.us>
2023-04-06 20:49:46 -0500

    thur apr 6

--- app/src/main/java/com/vipulasri/jetinstagram/ui/post/Post.kt
+++ app/src/main/java/com/vipulasri/jetinstagram/ui/post/Post.kt
@@ -1,5 +1,7 @@
 package com.vipulasri.jetinstagram.ui.post
 
+import android.graphics.drawable.Icon
+import android.inputmethodservice.Keyboard
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
@@ -7,13 +9,16 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,7 +31,7 @@ import com.vipulasri.jetinstagram.ui.theme.JetInstagramTheme
 
 @Composable
 fun ToolBar() {
-    Row(
+    Keyboard.Row(
         modifier = Modifier
             .fillMaxWidth()
             .height(56.dp)
@@ -39,9 +44,9 @@ fun ToolBar() {
             contentDescription = ""
         )
 
-        Text(text = "New Post", style = MaterialTheme.typography.h4)
+        Text(text = "New Post")
         Text(
-            text = "Share", style = MaterialTheme.typography.h4,
+            text = "Share",
             color = Color.Blue//color = MaterialTheme.colors.onBackground
         )
     }
@@ -59,10 +64,19 @@ fun PostPic(/*post: Post*/) {
             contentDescription = null,
             modifier = Modifier.size(60.dp)
         )
-        Text(
-            text = "Write a caption...",// stringResource(text),
-            color = Color.LightGray,
-            modifier = Modifier.padding(horizontal = 16.dp)
+        TextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .heightIn(min = 56.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = MaterialTheme.colors.surface
+            ),
+            placeholder = {
+                Text("Write a caption...")
+            }
         )
     }
     Divider()
@@ -73,37 +87,24 @@ fun TagPeople(
     titleHeaderPost: String,
     function: () -> Unit//for the click
 ) {
+    var expanded by remember { mutableStateOf(false) }
     Row(
         modifier = Modifier
-            .fillMaxWidth()
-            // .height(56.dp)
-          //  .padding(4.dp),
-       , horizontalArrangement = Arrangement.SpaceBetween,
+            .fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(text = titleHeaderPost)
         IconButton(
-            onClick = function
-            /*Toast.makeText(withContext(cont), "", Toast.LENGTH_SHORT).show()*/
+            onClick = { expanded = !expanded }
         ) {
             Icon(
-                ImageVector.vectorResource(id = R.drawable.ic_post_header_collapse),
+//                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                if (expanded)
+                    ImageVector.vectorResource(id = R.drawable.ic_keyboard_arrow_down)
+                else ImageVector.vectorResource(id = R.drawable.ic_post_header_collapse),
                 contentDescription = ""
             )
         }
-    }
-}
-
-@Composable
-fun AddFundraiser(
-    titleHeaderPost: String,//Add Fundraiser
-    function: () -> Unit
-) {
-    Row(
-        modifier = Modifier,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        TagPeople(titleHeaderPost = titleHeaderPost, function)
     }
 }
 
@@ -114,18 +115,20 @@ fun AddLocation(//=> could be a slot
     function: () -> Unit,
     modifier: Modifier
 ) {
-    Column(modifier = modifier) {
-        Row(
-            modifier = modifier,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            TagPeople(titleHeaderPost = titleHeaderPost, function)
-        }
-        Text(
-            text = "Tap here to show locations suggestions",// stringResource(text),
-            color = Color.LightGray,
-            modifier = Modifier//.padding(4.dp)
+    Column(modifier = modifier.fillMaxWidth()) {
+        TagPeople(titleHeaderPost = titleHeaderPost, function)
+        TextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier
+                .fillMaxWidth(), colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = MaterialTheme.colors.surface
+            ),
+            placeholder = {
+                Text("Tap here to show locations suggestions")
+            }
         )
+        Divider()
     }
 }
 
@@ -135,12 +138,15 @@ fun ToggleRow(
     onCheckChange: ((Boolean) -> Unit)?,
     checked: Boolean,
     @DrawableRes image: Int?,//should be post's image => i don't know yet how to make it :(
-    modifier: Modifier = Modifier.padding(top=2.dp, bottom = 2.dp),
+    modifier: Modifier = Modifier.padding(top = 2.dp, bottom = 2.dp),
 ) {
     Surface(
         modifier = modifier.fillMaxWidth()
     ) {
-        Row(modifier = modifier.padding(end = 2.dp), verticalAlignment = Alignment.CenterVertically) {
+        Row(
+            modifier = modifier.padding(end = 2.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             image?.let {
                 Image(
                     painter = painterResource(it),
@@ -180,11 +186,11 @@ fun PostToOther(
                         onCheckChange = {},
                         checked = true,
                         image = R.drawable.ic_profile_image/*user.image*/
-                       // , modifier = modifier.padding(16.dp)
+                        // , modifier = modifier.padding(16.dp)
                     )
                 }
             }
-
+            Divider()
         }
     }
 }
@@ -216,9 +222,21 @@ fun PostScreen(modifier: Modifier = Modifier) {
             PostPic()
             TagPeople(titleHeaderPost = "Tag People") {}
             Divider()
-            AddLocation(titleHeaderPost = "Add Location", function = {  }, modifier = modifier)
+//            AddLocation(titleHeaderPost = "Add Location", function = {  }, modifier = modifier)
+            TagPeople(titleHeaderPost = "Add Location") {}
+            TextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier
+                    .fillMaxWidth(), colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = MaterialTheme.colors.surface
+                ),
+                placeholder = {
+                    Text("Tap here to show locations suggestions")
+                }
+            )
             Divider()
-            AddFundraiser(titleHeaderPost = "Add Fundraiser") {}
+            TagPeople(titleHeaderPost = "Add Fundraiser") {}
             Divider()
             PostToOther(users = listUsers)
             Divider()
@@ -236,7 +254,7 @@ private fun InstaBottom(modifier: Modifier = Modifier) {
                 .fillMaxWidth()
                 .padding(start = 4.dp)
                 .height(56.dp),
-              //  .padding(horizontal = 10.dp),
+            //  .padding(horizontal = 10.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             Text(text = "Advanced Settings", color = Color.LightGray)
@@ -258,7 +276,7 @@ fun Post() {
         Scaffold(
             bottomBar = { InstaBottom() }
         ) {
-            PostScreen(Modifier.padding(start = 4.dp))
+            PostScreen(Modifier)
         }
     }
 }
@@ -291,7 +309,7 @@ fun AddLocationPreview() {
 @Preview
 @Composable
 fun AddFundraiserPreview() {
-    AddFundraiser("Add Fundraiser", {})
+    TagPeople("Add Fundraiser", {})
 }
 
 @Preview
@@ -325,12 +343,12 @@ fun InstaBottomPreview() {
 
 @Preview
 @Composable
-fun PostScreenPreview(){
+fun PostScreenPreview() {
     PostScreen()
 }
 
 @Preview
 @Composable
-fun PostPreview(){
+fun PostPreview() {
     Post()
 }
